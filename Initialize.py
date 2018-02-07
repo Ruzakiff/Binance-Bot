0d58ae707712174c4d74ac12febd3036cead2f2d
Ruzakiff <ryanchenyang@gmail.com>
2018-02-07 11:40:44 -0500

    Update Initialize.py

--- Initialize.py
+++ Initialize.py
@@ -1,5 +1,33 @@
 #TODO
 #Continuiously get all data that is needed for bot to start
+#(rsi,sma for middle boll, etc)
+#feed data to indicators
+#Do not let bot start until all data is retreived, and keep updating until bot starts, all arrays must be filled
+#Give data to bot
+#should i write to different files for each one? (rsi, boll,etc) or same file
+from binance.client import Client
+import config
+import sys
+import json
+import numpy as np
+import time
+from indicators import *
+import matplotlib.pyplot as plt
+
+def login():
+	print "Connecting..."
+	try:
+		client = Client(config.client_key, config.client_secret)
+	except:
+		sys.exit("Failed to connect") #kills entire interpreter, so if 2 scripts, both die.
+	else:
+		print "Connected"
+	return client
+
+#main
+client=login();
+#Continuiously get all data that is needed for bot to start
+#(rsi,sma for middle boll, etc)
 #feed data to indicators
 #Do not let bot start until all data is retreived, and keep updating until bot starts, all arrays must be filled
 #Give data to bot
