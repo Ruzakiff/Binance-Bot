6a88c32b6a74ae129a9c5a6a83331197bb1eabbf
Ruzakiff <ryanchenyang@gmail.com>
2018-02-11 00:50:03 -0500

    Update doggo4.py

--- doggo4.py
+++ doggo4.py
@@ -1,4 +1,26 @@
-import talib as indicator
+from binance.client import Client
+import talib
 import numpy as np
 import time
 from indicators import *
+buyAmount=0
+sellAmount=0
+def login():
+	print "Connecting..."
+	try:
+		client = Client(config.client_key, config.client_secret)
+	except:
+		sys.exit("Failed to connect") #kills entire interpreter, so if 2 scripts, both die.
+	else:
+		print "Connected"
+	return client
+
+#main
+client=login();
+while 1:
+	if(rsi[len(rsi)-1]<=30):
+  order = client.order_market_buy(
+      symbol='ETHBTC',
+      quantity=buyAmount)
+    
+    
