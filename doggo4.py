650b350bb010a76fb41a63c9f6d59b5b88d64fa8
Ruzakiff <ryanchenyang@gmail.com>
2018-02-11 01:17:18 -0500

    Update doggo4.py

--- doggo4.py
+++ doggo4.py
@@ -25,29 +25,34 @@ def Sell(symbol,amount):
 		symbol=symbol,
 		quantity=Amount)
 while 1:
-	if(rsi[len(rsi)-1]<=30):
-		Buy('ETHBTC',buyAmount)
-	if(rsi[len(rsi)-1)>=70):
-		Sell('ETHBTC',sellAmount)
-	if(lMacD[len(lMacD)-2]>hMacD[len(hMacD)-2]:#second previous, small higher than big
-	   	if(lMacD[len(lMacD)-1]<=hMacD[len(hMacD)-1):#crossed over, or at line
-			Sell('ETHBTC',sellAmount)
-	if(lMacD[len(lMacD)-2]<hMacD[len(hMacD)-2]:#second previous, small lower than big
-	   	if(lMacD[len(lMacD)-1]>=hMacD[len(hMacD)-1):#crossed over, or at line
+	try:#catch actual exceptions, do before actual launch
+		if(rsi[len(rsi)-1]<=30):
 			Buy('ETHBTC',buyAmount)
+		if(rsi[len(rsi)-1)>=70):
+			Sell('ETHBTC',sellAmount)
+		if(lMacD[len(lMacD)-2]>hMacD[len(hMacD)-2]:#second previous, small higher than big
+		   	if(lMacD[len(lMacD)-1]<=hMacD[len(hMacD)-1):#crossed over, or at line
+				Sell('ETHBTC',sellAmount)
+		if(lMacD[len(lMacD)-2]<hMacD[len(hMacD)-2]:#second previous, small lower than big
+		   	if(lMacD[len(lMacD)-1]>=hMacD[len(hMacD)-1):#crossed over, or at line
+				Buy('ETHBTC',buyAmount)
 	#Possible sell signals:The CCI crosses above 100 and has started to curve downwards.
 	#Possible buy signals:The CCI crosses below -100 and has started to curve upwards.
 	#Taken from stockcharts, didnt know
 	#also, it most things refer to line/curve, but we are just checking previous point before
 	#which for some reason could be like really weird or outlier, so not accurate, do we care?
 	#or is that what is supposed to be good, for sniping and shit
-	if(cci[len(cci)-2]>100):
-		if(cci[len(cci)-1]<cci[len(cci)-2]):#curve down
-			Sell('ETHBTC',sellAmount)
-	if(cci[len(cci)-2]<-100):
-		if(cci[len(cci)-1>cci[len(cci)-2]):#curve up
-		       Buy('ETHBTC',buyAmount)
-	time.sleep(1)
+		if(cci[len(cci)-2]>100):
+			if(cci[len(cci)-1]<cci[len(cci)-2]):#curve down
+				Sell('ETHBTC',sellAmount)
+		if(cci[len(cci)-2]<-100):
+			if(cci[len(cci)-1>cci[len(cci)-2]):#curve up
+			       Buy('ETHBTC',buyAmount)
+		time.sleep(1)
+	except:
+		print "holl up"
+	else:
+		print "gucci"
 					     
 	       
        
