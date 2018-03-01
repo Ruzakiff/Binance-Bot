186cc48c167a9c467c49bbcf2178b1209dfa9fb1
Ruzakiff <ryanchenyang@gmail.com>
2018-03-01 16:17:21 -0500

    Update doggo4.py

--- doggo4.py
+++ doggo4.py
@@ -2,10 +2,10 @@ from binance.client import Client
 import numpy as np
 import time
 import talib
-#from indicators import *
-#buyAmount=0
-amount=0
-#sellAmount=buyAmount
+import sys
+import config
+reading=True
+amount=10
 rsiBuy=0
 macBuy=0
 cciBuy=0
@@ -17,8 +17,17 @@ gainCounter=0
 lossCounter=0
 avgGain=0
 avgLoss=0
-sma
+sma=np.array([])
+ema=np.array([])
+rsi=np.array([])
+cci=np.array([])
+#2880
+lengthTime=1000
+ethbtc_close=np.array([])
+ethbtc_high=np.array([])
+ethbtc_low=np.array([])
 #TODO GET ACCOUNT BALANCE
+accountBalance=1000
 def login():
 	print "Connecting..."
 	try:
@@ -28,9 +37,6 @@ def login():
 	else:
 		print "Connected"
 	return client
-
-#main
-client=login();
 def Buy(symbol,amount):
 	order = client.order_market_buy(
 		symbol=symbol,
@@ -39,85 +45,160 @@ def Sell(symbol,amount):
 	order = client.order_market_sell(
 		symbol=symbol,
 		quantity=amount)
-datafile=open("/Users/ryan/Desktop/inodawey/ethbtc_price.txt", "r")
+
+
+#main
+client=login()
+datafile=open("/Users/ryan/Desktop/doggo4/Klines.txt", "r")
 while 1:
+	currentBalance=client.get_asset_balance(asset="ETH")
+	reading=True
 	#update
-	where = datafile.tell()
-	line = datafile.readline()
-	if not line:
-		#   time.sleep(1)
-		datafile.seek(where)
-    	else:
-		ethbtc_price=np.append(ethbtc_price,float(line[21:31]) #np array
-    		#ethbtc_price.append(float(line[21:31]))
-    		if(len(ethbtc_price)>lengthTime):
-    		#del ethbtc_price[0]
-			ethbtc_price=np.delete(ethbtc_price,0) #has to be numpy, talib wants numpy
-			       		       
+	while reading:
+		where = datafile.tell()
+		line = datafile.readline()
+		if not line:
+			reading=False
+			datafile.seek(where)
+		else:
+		#print line
+			reading=True
+			print line
+			ethbtc_high=np.append(ethbtc_high,float(line[32:42]))
+			ethbtc_low=np.append(ethbtc_low,float(line[46:56]))
+			ethbtc_close=np.append(ethbtc_close,float(line[60:70]))
+			if(len(ethbtc_close)>lengthTime):
+				ethbtc_close=np.delete(ethbtc_close,0) #has to be numpy, talib wants numpy
+			if(len(ethbtc_high)>lengthTime):
+				ethbtc_high=np.delete(ethbtc_high,0) #has to be numpy, talib wants numpy
+			if(len(ethbtc_low)>lengthTime):
+				ethbtc_low=np.delete(ethbtc_low,0) #has to be numpy, talib wants numpy
+			if(len(ethbtc_high)<lengthTime or len(ethbtc_close)<lengthTime or len(ethbtc_low)<lengthTime):
+				reading=True
+			else:
+				reading=False
+			#print ethbtc_high
+		#print line[32:42]
+		#print line[46:56]
+		#print line[60:70]
+			#print reading
+    		#print len(ethbtc_close)
+	
+	#print "Not Reading"
 	#indicators
-	if(len(ethbtc_price)>lengthTime):
-    		#del ethbtc_price[0]
-		ethbtc_price=np.delete(ethbtc_price,0) #has to be numpy, talib wants numpy
-		sma=talib.SMA(ethbtc_price,timeperiod=lengthTime)
-		ema=talib.EMA(ethbtc_price,timeperiod=lengthTime)
-		rsi=talib.RSI(ethbtc_price,timeperiod=lengthTime)
-		hMacD,mMacD,lMacD=talib.MACD(ethbtc_price,fastperiod=12, slowperiod=26, signalperiod=9)#default intervals
-		if(len(ethbtc_high)>=1500 and len(ethbtc_low)>=1500):
-			cci=talib.CCI(ethbtc_high,ethbtc_low,ethbtc_close,timeperiod=lengthTime)
-	#execute
-	try:#catch actual exceptions, do before actual launch
-		currentPrice=ethbtc_price[len(ethbtc_price)-1]
-		if(len(buyValue)==len(sellValue)):
-			kelly=(gainCounter/lossCounter)-((1-(gainCounter/lossCounter))/(avgGain/avgLoss))
-		amount=kelly*accountBalance
-		if(rsi[len(rsi)-1]<=30):
-			rsiBuy=1
-			#Buy('ETHBTC',buyAmount)
-		if(rsi[len(rsi)-1)>=70):
-		       	rsiBuy=-1
-			#Sell('ETHBTC',sellAmount)
-		if(lMacD[len(lMacD)-2]>hMacD[len(hMacD)-2]:#second previous, small higher than big
-		   	if(lMacD[len(lMacD)-1]<=hMacD[len(hMacD)-1):#crossed over, or at line
-				macBuy=-1
-				#Sell('ETHBTC',sellAmount)
-		if(lMacD[len(lMacD)-2]<hMacD[len(hMacD)-2]:#second previous, small lower than big
-		   	if(lMacD[len(lMacD)-1]>=hMacD[len(hMacD)-1):#crossed over, or at line
-				macBuy=1
-				#Buy('ETHBTC',buyAmount)
-	#Possible sell signals:The CCI crosses above 100 and has started to curve downwards. 
-	#Possible buy signals:The CCI crosses below -100 and has started to curve upwards.
-	#signals buy if crosses zero line from - to +, sell vice versa
-	#also, it most things refer to line/curve, but we are just checking previous point before (this is why we would wanna use numpy if possible to apporixmate curve)
-	#which for some reason could be like really weird or outlier, so not accurate, do we care?
-	#or is that what is supposed to be good, for sniping and shit (supposed to be good, stength of cci is in outliers and momentum movement)
-		if(cci[len(cci)-2]>100):
-			if(cci[len(cci)-1]<cci[len(cci)-2]):#curve down
+	if(len(ethbtc_close)==lengthTime and len(ethbtc_low)==lengthTime and len(ethbtc_high)==lengthTime):
+	 	#maybe infinite size closing price, reading=true only while size <lengthTime. and keep appending, no delete
+	 	#or take last sma thing and append to actual sma array
+	 	tempsma=talib.SMA(ethbtc_close,timeperiod=lengthTime)
+	 	tempema=talib.EMA(ethbtc_close,timeperiod=lengthTime)
+	 	temprsi=talib.RSI(ethbtc_close,timeperiod=14)
+	 	hMacD,mMacD,lMacD=talib.MACD(ethbtc_close,fastperiod=12, slowperiod=26, signalperiod=9)#default intervals
+	 	tempcci=talib.CCI(ethbtc_high,ethbtc_low,ethbtc_close,timeperiod=lengthTime)
+	 	#print temprsi
+	 	sma=np.append(sma,tempsma[len(tempsma)-1])
+	 	ema=np.append(ema,tempema[len(tempema)-1])
+	 	##not entire rsi, just appended last cause lazy. it should be oki
+	 	rsi=np.append(rsi,temprsi[len(temprsi)-1])
+	 	cci=np.append(cci,tempcci[len(tempcci)-1])
+	 	#print rsi
+	 	if(rsi[len(rsi)-1]<=30):
+	 		rsiBuy=1
+	 	if(rsi[len(rsi)-1]>=70):
+	 		rsiBuy=-1
+	 	if(cci[len(cci)-2]>100):
+	 		if(cci[len(cci)-1]<cci[len(cci)-2]):#curve down
 				cciBuy=-1
-			      	#Sell('ETHBTC',sellAmount)
-		if(cci[len(cci)-2]<-100):
-			if(cci[len(cci)-1>cci[len(cci)-2]):#curve up
+		if(cci[len(cci)-2]<=100):
+			if(cci[len(cci)-1]>cci[len(cci)-2]):
 				cciBuy=1
-		       		#Buy('ETHBTC',buyAmount)
 		if(cci[len(cci)-2]<0):
-		       if(cci[len(cci)-1]>0):
+			if(cci[len(cci)-1]>0):
 				cciBuy=1
-		       		#Buy('ETHBTC',buyAmount)
 		if(cci[len(cci)-2]>0):
-		       if(cci[len(cci)-1]<0):
+			if(cci[len(cci)-1]<0):
 				cciBuy=-1
-	       			#Sell('ETHBTC',sellAmount)
-	      	if(cciBuy==1 and rsiBuy==1 and macBuy==1):
-		       		Buy('ETHBTC',amount)
-		       		buyValue=np.append(buyValue,currentPrice)
-		       		if(len(buyValue)>60):
-					buyValue=np.delete(buyValue,0)
-	       	if(cciBuy==-1 and rsiBuy==-1 and macBuy==-1):
-		       		Sell('ETHBTC',amount)
-		       		sellValue=np.append(sellValue,currentPrice)
-			       	if(len(sellValue)>60):
-		       			sellValue=np.delete(sellValue,0)
-		time.sleep(1)
-	except:
-		print "holl up"
+		if(cciBuy==0 or rsiBuy==0):
+			print "Not Doing Anything"
+			print "cci",cci[len(cci)-1]
+			print "rsi",rsi[len(rsi)-1]
+		if(cciBuy==1 and rsiBuy==1):
+			cciBuy=0
+			rsiBuy=0
+			print "Buying:",ethbtc_close[len(ethbtc_close)-1]
+			print "cci",cci[len(cci)-1]
+			print "rsi",rsi[len(rsi)-1]
+	# 	       		Buy('ETHBTC',amount)
+	# 	       		buyValue=np.append(buyValue,currentPrice)
+	# 	       		if(len(buyValue)>60):
+	# 				buyValue=np.delete(buyValue,0)
+		if(cciBuy==-1 and rsiBuy==-1):
+			cciBuy=0
+			rsiBuy=0
+			print "Selling:",ethbtc_close[len(ethbtc_close)-1]
+			print "cci",cci[len(cci)-1]
+			print "rsi",rsi[len(rsi)-1]
+
+	# 	       		Sell('ETHBTC',amount)
+	# 	       		sellValue=np.append(sellValue,currentPrice)
+	# 		       	if(len(sellValue)>60):
+	# 	       			sellValue=np.delete(sellValue,0)
+
+	#execute
+	# try:#catch actual exceptions, do before actual launch
+	# 	currentPrice=ethbtc_price[len(ethbtc_price)-1]
+	# 	if(len(buyValue)==len(sellValue)):
+	# 		kelly=(gainCounter/lossCounter)-((1-(gainCounter/lossCounter))/(avgGain/avgLoss))
+	# 	amount=kelly*accountBalance
+	# 	if(rsi[len(rsi)-1]<=30):
+	# 		rsiBuy=1
+	# 		#Buy('ETHBTC',buyAmount)
+	# 	if(rsi[len(rsi)-1)>=70):
+	# 	       	rsiBuy=-1
+	# 		#Sell('ETHBTC',sellAmount)
+	# 	if(lMacD[len(lMacD)-2]>hMacD[len(hMacD)-2]:#second previous, small higher than big
+	# 	   	if(lMacD[len(lMacD)-1]<=hMacD[len(hMacD)-1):#crossed over, or at line
+	# 			macBuy=-1
+	# 			#Sell('ETHBTC',sellAmount)
+	# 	if(lMacD[len(lMacD)-2]<hMacD[len(hMacD)-2]:#second previous, small lower than big
+	# 	   	if(lMacD[len(lMacD)-1]>=hMacD[len(hMacD)-1):#crossed over, or at line
+	# 			macBuy=1
+	# 			#Buy('ETHBTC',buyAmount)
+	# #Possible sell signals:The CCI crosses above 100 and has started to curve downwards. 
+	# #Possible buy signals:The CCI crosses below -100 and has started to curve upwards.
+	# #signals buy if crosses zero line from - to +, sell vice versa
+	# #also, it most things refer to line/curve, but we are just checking previous point before (this is why we would wanna use numpy if possible to apporixmate curve)
+	# #which for some reason could be like really weird or outlier, so not accurate, do we care?
+	# #or is that what is supposed to be good, for sniping and shit (supposed to be good, stength of cci is in outliers and momentum movement)
+	# 	if(cci[len(cci)-2]>100):
+	# 		if(cci[len(cci)-1]<cci[len(cci)-2]):#curve down
+	# 			cciBuy=-1
+	# 		      	#Sell('ETHBTC',sellAmount)
+	# 	if(cci[len(cci)-2]<-100):
+	# 		if(cci[len(cci)-1>cci[len(cci)-2]):#curve up
+	# 			cciBuy=1
+	# 	       		#Buy('ETHBTC',buyAmount)
+	# 	if(cci[len(cci)-2]<0):
+	# 	       if(cci[len(cci)-1]>0):
+	# 			cciBuy=1
+	# 	       		#Buy('ETHBTC',buyAmount)
+	# 	if(cci[len(cci)-2]>0):
+	# 	       if(cci[len(cci)-1]<0):
+	# 			cciBuy=-1
+	#        			#Sell('ETHBTC',sellAmount)
+	#       	if(cciBuy==1 and rsiBuy==1 and macBuy==1):
+	# 	       		Buy('ETHBTC',amount)
+	# 	       		buyValue=np.append(buyValue,currentPrice)
+	# 	       		if(len(buyValue)>60):
+	# 				buyValue=np.delete(buyValue,0)
+	#        	if(cciBuy==-1 and rsiBuy==-1 and macBuy==-1):
+	# 	       		Sell('ETHBTC',amount)
+	# 	       		sellValue=np.append(sellValue,currentPrice)
+	# 		       	if(len(sellValue)>60):
+	# 	       			sellValue=np.delete(sellValue,0)
 	else:
-		print "gucci"
+		print "Size Not Large Enough, Waiting For Update"
+	time.sleep(1)
+	# except:
+	# 	print "holl up"
+	# else:
+	# 	print "gucci"
