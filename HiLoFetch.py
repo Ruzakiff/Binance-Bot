693ee43269018525ff131c2f268900a4eab009f2
Ruzakiff <ryanchenyang@gmail.com>
2018-03-24 17:52:13 -0400

    Update HiLoFetch.py

--- HiLoFetch.py
+++ HiLoFetch.py
@@ -1,13 +1,12 @@
 from binance.client import Client
-import numpy as np
 import config
 import sys
 import json
 import time
+pair='ADAETH'
+path="/Users/ryan/Desktop/Doggo4/"+pair+".txt"
 def login():
 	print "Connecting..."
-	#print config.client_key
-	#print config.client_secret
 	try:
 		client = Client(config.client_key, config.client_secret)
 	except:
@@ -17,15 +16,16 @@ def login():
 	return client
 
 #main
-client=login();
+client=login()
 while 1:
 	try:
-		symbol_ticker=json.dumps(client.get_klines(symbol='ETHBTC', interval=Client.KLINE_INTERVAL_1MINUTE,limit=1))
-		[[1519654380000,"0.08511500","0.08525000","0.08511200","0.08514600","17.33200000",1519654439999,"1.47620423",62,"6.80200000","0.57963530","0"]]
-		symbol_ticker=symbol_ticker.replace("\"","")
-		symbol_ticker=symbol_ticker.replace("\b","")
-		symbol_ticker=symbol_ticker.replace(" ","")
-		with open ("/Users/ryan/Desktop/inodawey/HighLow.txt", "a") as outfile:				
+		symbol_ticker=json.dumps(client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_1MINUTE,limit=1))
+		print symbol_ticker
+		#[[1519654380000,"0.08511500","0.08525000","0.08511200","0.08514600","17.33200000",1519654439999,"1.47620423",62,"6.80200000","0.57963530","0"]]
+		#symbol_ticker=symbol_ticker.replace("\"","")
+		#symbol_ticker=symbol_ticker.replace("\b","")
+		#symbol_ticker=symbol_ticker.replace(" ","")
+		with open (path, "a") as outfile:				
 			outfile.write(symbol_ticker+"\n")
 		time.sleep(60)
 	except:
